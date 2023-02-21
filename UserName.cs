6c7e2983cfffe4348cb89effd42d2e7e6d8923ef
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-02-21 23:54:29 +0200

    Add files via upload

---
+++ UserName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTask6B
+{
+    internal class UserName
+    {
+        public UserName(int studentId, string username,
+            string password)
+        {
+            StudentId = studentId;
+            Username = username;
+            Password = password;
+        }
+
+        public int StudentId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        //Instance methods:        
+    }
+}
