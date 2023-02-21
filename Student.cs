a22c15215d3f3c8dd1811fb8668fb54c94067bd9
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-02-21 23:52:51 +0200

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿//Create and initialize the collections:
+using OOPTask6B;
+
+AllStudents allStudents = new AllStudents();
+
+AllUserNames allUserNames = new AllUserNames();
+
+//Joe has forgotten the passwork. Find it
+List<Result> results1 = allUserNames.FindPassword(allUserNames, allStudents, "Joe");
+
+if (results1.Count() > 0)
+    foreach (var item in results1)
+    {
+        Console.WriteLine("Student name: {0}, username: {1}, password: {2}",
+            item.Studentname, item.Username, item.Password);
+    }
+else
+    Console.WriteLine("No hit was found.");
+
+
+
+//Whose username is "morning..."
+List<Result> results2 = allStudents.FindStudent(allUserNames, allStudents, "morning");
+
+if (results2.Count() > 0)
+    foreach (var item in results2)
+    {
+        Console.WriteLine("Student name: {0}, username: {1}, password: {2}",
+            item.Studentname, item.Username, item.Password);
+    }
+else
+    Console.WriteLine("No hit was found.");
